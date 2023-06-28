ff81106196d3dda813a8c1d270660af22b9c9f98
Goldendenday <bieltikov99@mail.ru>
2023-06-28 11:45:22 +0300

    Make HW9-11

---
+++ TASK10 HW/Program.cs
@@ -0,0 +1,14 @@
+﻿ Random random = new Random();
+ int[] arr = new int[10];
+ int sum = 0;
+for (int i = 0; i < arr.Length; i++) {
+    arr[i] = random.Next(1, 100);
+    Console.Write(arr[i] + " ");
+        }
+
+for (int i = 1; i < arr.Length; i+=2)
+ {
+sum += arr[i];
+ }
+Console.WriteLine("\nСумма элементов на нечетных позициях: " + sum);
+   
