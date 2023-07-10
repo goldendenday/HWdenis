2248bf9e03ba8342026087b7a17464dce65a5d87
Goldendenday <bieltikov99@mail.ru>
2023-07-10 22:10:53 +0300

    Make HW

---
+++ TASk17/Program.cs
@@ -0,0 +1,27 @@
+﻿static int Ackermann(int m, int n)
+    {
+        if (m == 0)
+        {
+            return n + 1;
+        }
+        else if (n == 0)
+        {
+            return Ackermann(m - 1, 1);
+        }
+        else
+        {
+            return Ackermann(m - 1, Ackermann(m, n - 1));
+        }
+    }
+
+    {
+        Console.Write("Введите число m: ");
+        int m = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите число n: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int result = Ackermann(m, n);
+
+        Console.WriteLine($"Значение функции Аккермана для m={m} и n={n}: {result}");
+    }
